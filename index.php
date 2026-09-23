3c883c20e08e4e0ae6acb102fdcfe0b57af9c8b4
Sabila Rosad <sabilarosad76@gmail.com>
2026-09-23 12:41:47 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Law Firm</title>
+  </head>
+  <body>
+    <div id="app"></div>
+
+    <!-- Hubungkan ke file JS utama kamu -->
+    <script type="module" src="/resources/js/app.js"></script>
+  </body>
+</html>
